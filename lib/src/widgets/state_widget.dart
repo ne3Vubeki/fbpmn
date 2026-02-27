b30436916f5cc17afbb6f6927360eb7fbc28dbd2
vmaltsev@diasoft.ru <123@Kas8>
2026-02-27 20:01:06 +0300

    qtskt-655401 фиксация доработок

--- lib/src/widgets/state_widget.dart
+++ lib/src/widgets/state_widget.dart
@@ -1,30 +1,27 @@
-import 'dart:async';
+// import 'dart:async';
 
 import 'package:flutter/widgets.dart';
 
 mixin StateWidget<T extends StatefulWidget> on State<T> {
-  Timer? _timer; // Добавляем поле класса для хранения таймера
+  //   Timer? _timer; // Добавляем поле класса для хранения таймера
 
-  void timeoutSetState({
-    Duration? duration, 
-    VoidCallback? callback,
-  }) {
-    _timer?.cancel(); // Отменяем предыдущий таймер если есть
-    
-    _timer = Timer(duration ?? Duration.zero, () {
-      if (mounted) {
-        setState(() {
-          callback?.call();
-        });
-      }
-      _timer = null;
-    });
+  void timeoutSetState({Duration? duration, VoidCallback? callback}) {
+    if (mounted) {
+      setState(() {
+        callback?.call();
+      });
+    }
+    //    _timer?.cancel(); // Отменяем предыдущий таймер если есть
+    //
+    //     _timer = Timer(duration ?? Duration.zero, () {
+    //       _timer = null;
+    //     });
   }
-  
+
   @override
   void dispose() {
-    _timer?.cancel();
-    _timer = null;
+    //     _timer?.cancel();
+    //     _timer = null;
     super.dispose();
   }
 }
